1c3cdaf42a43f003550dc35b3ab66d22134c53f7
EmilioAK <emilio.alvarez96@gmail.com>
2019-11-17 03:05:49 +0100

    Initial commit

---
+++ mentalcalc.py
@@ -0,0 +1,34 @@
+import time
+import random
+
+def headcount(question, correct):
+    start = time.time()
+    answer = input(question)
+    end = time.time()
+
+    answer_time = end-start
+
+    if answer == correct:
+        return answer_time
+    else:
+        return 0
+
+questions = [["5+2= ", 7], ["6+2= ", 8], ["4+2= ", 6], ["7+2= ", 9], ["8+7= ", 15], ["5+7= ", 12], ["5+8= ", 13], ["6+7= ", 13], ["8+6= ", 14]]
+random.shuffle(questions)
+
+#-------------
+
+total_solve_time = 0
+for question in questions:
+    solve_time = headcount(question[0], question[1])
+    if solve_time == 0:
+        print("Wrong. Start over.")
+        # Brings it back to 0 so I can make this the condition for faliure in the last if
+        total_solve_time = 0
+        break
+    else:
+        total_solve_time += solve_time
+        print(str(total_solve_time) + " seconds of solve time")
+
+if total_solve_time:
+    print("\nTotal time: " + str(total_solve_time))
